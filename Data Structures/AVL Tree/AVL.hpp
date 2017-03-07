89e9e5e4a2ba48a71b96f2fb41eae8576adb20fc
Oran Can Ören <orancanoren@gmail.com>
2017-03-07 21:54:25 +0300

    Create AVL.hpp

---
+++ Data Structures/AVL Tree/AVL.hpp
@@ -0,0 +1,44 @@
+#ifndef _BST_H
+#define _BST_H
+
+struct node {
+	node * left;
+	node * right;
+	int key;
+	int height;
+
+	node(int item) : key(item), height(0) { }
+};
+
+// AVL tree
+
+class BST {
+public:
+	BST();
+	~BST();
+
+	void insert(int item);
+	void remove(int item);
+
+	bool search(int item) const;
+	int minimum() const;
+	int maximum() const;
+	void makeEmpty();
+private:
+	node * root;
+
+	void rotateWithLeftChild(node *&);
+	void rotateWithRightChild(node *&);
+	void doubleWithLeftChild(node *&);
+	void doubleWithRightChild(node *&);
+
+	int height(node *) const; // returns the height of a node | returns -1 if nullptr
+	void insert(int item, node *&);
+	void remove(int item, node *&);
+	void balance(node *& subroot);
+	void makeEmpty(node *&);
+	int minimum(node *) const;
+	int maximum(node *) const;
+};
+
+#endif
