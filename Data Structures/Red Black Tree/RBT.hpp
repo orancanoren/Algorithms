99ebe5e2163772b8d470f2256c6e48b983daf093
Oran Can Ören <orancanoren@gmail.com>
2017-03-13 12:49:50 +0300

    Update RBT.hpp

--- Data Structures/Red Black Tree/RBT.hpp
+++ Data Structures/Red Black Tree/RBT.hpp
@@ -1,9 +1,19 @@
 #ifndef _RBT_H
 #define _RBT_H
 
+struct node {
+	node * parent;
+	bool red;
+	node * left;
+	node * right;
+	int key;
+	int size; // augmented field
 
+	node() : red(false), left(nullptr), right(nullptr), parent(nullptr), size(0) { } // dummy leaf node
+	node(int item) : red(false), left(nullptr), right(nullptr), key(item), parent(nullptr), size(1) { }
+}; 
 
-// Red Black Tree
+// Dynamic Set using an augmented Red Black Tree
 
 class RBT {
 public:
@@ -13,35 +23,35 @@ public:
 	void insert(int item);
 	void remove(int item);
 
-	int minimum();
-	int maximum();
+	int minimum() const;
+	int maximum() const;
+	int successor(int item) const;
+	int predecessor(int item) const;
+	int rank(int item) const; // gets the i'th order statistics in the set
+
 	void makeEmpty();
 private:
-	struct node {
-	node * parent;
-	bool red;
-	node * left;
-	node * right;
-	int key;
-
-	node() : red(false), left(nullptr), right(nullptr), parent(nullptr) { left = nil;} // dummy leaf node
-	node(int item) : red(false), left(nullptr), right(nullptr), key(item), parent(nullptr) { }
-}; 
-
 	node * root;
 	node * nil;
 
 	void makeEmpty(node *);
 	void insert(node *);
-	void remove(node *);
 
-	int minimum(node *);
-	int maximum(node *);
-
-	void rotate_right(node * around);
-	void rotate_left(node * around);
+	// Utility Functions
+	void rotate_right(node *& around);
+	void rotate_left(node *& around);
 	void BST_insert(node * nn);
-	void recolor(node * around);
+	node * BST_remove(int item, node * n);
+	void insertFixup(node * around);
+
+	// Dynamic Set operations
+	node * maximum(node * subRoot) const;
+	node * minimum(node * subRoot) const;
+	node * successor(node *& subRoot) const;
+	node * predecessor(node *& subRoot) const;
+	node * find(int key) const;
+	int rank(node * of) const; // returns the rank of a node
+	int orderStatistic(node * subRoot, int i); // returns the i'th order statistic
 };
 
 #endif
